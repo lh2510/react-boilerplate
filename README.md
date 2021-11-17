9085ee8d9c0a6d86a4e9526b13f7c4d07e4a24d4
lh2510 <lh2510@nyu.edu>
2021-11-17 11:53:51 -0500

    index.js index.css App.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-boilerplate
+
 react-boilerplate
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className='app'>
+			<h1>Hello</h1>
+			<hr />
+			<p>Welcome to Lin's React Boilerplate</p>
+		</div>
+	);
+}
+
+export default App;
